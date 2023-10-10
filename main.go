10535449273846de473fde4f643f4a4ad8cade22
Gauthama Siddharth <49897362+GauthamaSid@users.noreply.github.com>
2023-10-10 20:08:12 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,61 @@
+
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"strconv"
+
+	"github.com/miekg/dns"
+	"github.com/redis/go-redis/v9"
+)
+
+var client = redis.NewClient(&redis.Options{
+	Addr:     "localhost:6379",
+	Password: "", // no password set
+	DB:       0,  // use default DB
+})
+
+var domainsToAddresses map[string]string = map[string]string{
+	"google.com.":     "1.2.3.4",
+	"cloudflare.com.": "1.1.1.1",
+}
+
+type handler struct{}
+
+func (this *handler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
+	ctx := context.Background()
+	msg := dns.Msg{}
+	msg.SetReply(r)
+	switch r.Question[0].Qtype {
+	case dns.TypeA:
+		msg.Authoritative = true
+		domain := msg.Question[0].Name
+		// address, errs := client.Get(ctx, domain).Result()
+		address, errs := client.Do(ctx, "get", domain).Result()
+		if errs == nil {
+			log.Println(address)
+			msg.Answer = append(msg.Answer, &dns.A{
+				Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
+				A:   net.ParseIP(address.(string)),
+			})
+		}
+		if errs != nil {
+			msg.Answer = append(msg.Answer, &dns.A{
+				Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
+				A:   net.ParseIP("8.8.8.8"),
+			})
+		}
+	}
+	w.WriteMsg(&msg)
+}
+
+func main() {
+	srv := &dns.Server{Addr: ":" + strconv.Itoa(53), Net: "udp"}
+	srv.Handler = &handler{}
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("Failed to set udp listener %s\n", err.Error())
+	}
+	log.Println()
+}
